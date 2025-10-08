9b210a5972b4385fcfbbd2552c1f284c108a2730
jose1991rojas <developer.halloworld@gmail.com>
2025-10-08 15:54:44 -0400

    I add my exercise number 9 in the folder ---> C++ Expresiones y Operadores

---
+++ practicas/Nivel Basico C++/2. C++ Expresiones y Operadores/ejercicio 9.cpp
@@ -0,0 +1,23 @@
+/*9. Realice un programa que calcule el
+ valor que toma la siguiente funcion para 
+ unos valores dados de x e y:
+ f(x,y)= sqrt(x)/pow(y,2)-1*/
+ 
+ #include <iostream>
+ #include math.h
+ 
+ using namespace std;
+ 
+ int main(){
+ 	float x, y, resultado=0;
+ 	
+ 	cout<<"Digite el valor de X: "; cin>>x;
+ 	cout<<"Digite el valor de Y: "; cin>>Y;
+ 	
+ 	resultado = (sqrt(x))/(pow(y,2)-1);
+ 	
+ 	cout.precision(3);
+ 	cout<<"El resultado es: "<<resultado<<endl;
+ 	
+ 	return 0;
+ }
