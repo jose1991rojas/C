f5c84141b0ba97da9d94b016a74bd2f8bbed74e5
jose1991rojas <developer.halloworld@gmail.com>
2026-05-05 14:37:25 -0400

    feat(add strlwr() and 7 excercise): add function strlwr(); and excercise 7

---
+++ practicas/nivel-basico-c++/7.Cadenas/ejercicio-7.-Cadena-de-mayuscula-aminuscula.cpp
@@ -0,0 +1,39 @@
+/*
+Ejercicio 7:
+
+Pedir su nombre al usuario en MAYÚSCULA,
+si su nombre comienza por la letra A,
+convertir su nombre a minusculas, caso
+contrario no convertirlo.
+*/
+
+#include<iostream>
+#include<string.h>
+#include<ruzza.h>
+//#include<conio.h>
+//#include<stdlib.h>
+
+using namespace std;
+
+int main(){
+    char nombre[100];
+
+    cout << "Digite su nombre en MAYÚSCULAS: ";
+    cin.getline(nombre , 100);
+
+    if(nombre[0] == 'A'){
+        strlwr(nombre[100]&);
+        cout << "\n";
+        cout << nombre<< endl;
+    }
+    else{
+        cout << "\nSu nombre no comienza con A: " << nombre << endl;
+    }
+
+    cout << "\n";
+    cout << nombre[0] << endl;
+
+    //getch();
+    //system("pause");
+    return 0;
+}
---
+++ practicas/nivel-basico-c++/7.Cadenas/funcion-strlwr.cpp
@@ -0,0 +1,24 @@
+/*
+Pasar una palabra a minusculas - Funcion strlwr();
+*/
+
+#include<iostream>
+#include<string.h>
+//#include<conio.h>
+//#include<stdlib.h>
+
+using namespace std;
+
+int main(){
+    char palabra[] = "VENEZUELA";
+
+    strwlr(palabra);
+
+    cout << palabra << endl;
+
+    //getch();
+    //system("pause");
+    return 0;
+}
+
+
